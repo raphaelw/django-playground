9843d932b0365a0d2e3e53e3667f7cf4a1e3d082
raphaelw <raphaelw@users.noreply.github.com>
2025-04-27 04:18:15 +0200

    extend routes

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.urls import reverse
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    results_url_path = reverse(results, kwargs={"question_id":123})
+    return HttpResponse(f"""You're looking at question {question_id}. Results: {results_url_path}""")
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
